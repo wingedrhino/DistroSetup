e29b9e060a3390a12354cd33d6a127b02aa262da
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    Reformatted golang-packages.sh; added gometalinter

--- setup-helpers/golang-packages.sh
+++ setup-helpers/golang-packages.sh
@@ -1,27 +1,21 @@
 printf "Begin Go Package Install Script"
 
-printf "\n\n"
-printf "dep - Go dependency tool"
-printf "\n\n"
+printf "\n\ndep - Go dependency tool\n\n"
 go get -u -v -t github.com/golang/dep/cmd/dep
 
-printf "\n\n"
-printf "xsdgen; make sure you remove UseFieldNames() from config and recompile"
-printf "\n\n"
+printf "\n\ngometalinter - lint 'em all\n\n"
+go get -u -v -t github.com/alecthomas/gometalinter
+gometalinter --install
+
+printf "\n\nxsdgen - structs from XSD\n\n"
 go get -u -v -t aqwari.net/xml/cmd/xsdgen
 
-printf "\n\n"
-printf "protoc-gen-go"
-printf "\n\n"
+printf "\n\nprotoc-gen-go\n\n"
 go get -u -v -t github.com/golang/protobuf/protoc-gen-go
-printf "\n\n"
-printf "protoc-gen-grpc-gateway"
-printf "\n\n"
+printf "\n\nprotoc-gen-grpc-gateway\n\n"
 go get -u -v -t github.com/grpc-ecosystem/grpc-gateway/protoc-gen-grpc-gateway
-printf "\n\n"
-printf "protoc-gen-swagger"
-printf "\n\n"
+printf "\n\nprotoc-gen-swagger\n\n"
 go get -u -v -t github.com/grpc-ecosystem/grpc-gateway/protoc-gen-swagger
 
 printf "\n\n"
-printf "End Go Package Install Script"
+printf "\n\nEnd Go Package Install Script\n\n"
