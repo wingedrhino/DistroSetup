3a20a6e74993bf10a5fc4a96db8352a147c8cc04
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Fixed error in Golang version name in setup

--- setup-helpers/Fedora/dev-server-setup.sh
+++ setup-helpers/Fedora/dev-server-setup.sh
@@ -128,7 +128,7 @@ dnf autoremove -y
 printf "\n\nInstall Golang\n"
 rm -r /usr/local/go
 wget -c https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.11.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
 echo 'export PATH="/usr/local/go/bin:$PATH"' > /etc/profile.d/golangpath.sh
 
 printf "\n\nInstall Hugo w/ Snap\n"
