f9919766cfb93bdea7be3f8d1af9ac15c2bfcadc
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Ubuntu: Upgrade Anaconda

--- Ubuntu/anaconda.sh
+++ Ubuntu/anaconda.sh
@@ -4,6 +4,6 @@ printf "Running commands as user $USER"
 
 printf "\n\nInstall Anaconda\n"
 rm -r $HOME/ext/bin/anaconda
-wget -c https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-bash Anaconda3-2019.10-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
+wget -c https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+bash Anaconda3-2020.02-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
 
