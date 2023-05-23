5e4de8b514365b4c279072c60b28bcc7502ffe33
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Updated Ubuntu Setup's Anaconda Version

--- setup-helpers/Ubuntu/as-user.sh
+++ setup-helpers/Ubuntu/as-user.sh
@@ -27,5 +27,5 @@ curl -L https://raw.githubusercontent.com/wingedrhino/DistroSetup/trunk/dotfiles
 
 printf "\n\nInstall Anaconda\n"
 rm -r $HOME/ext/bin/anaconda
-wget -c https://repo.anaconda.com/archive/Anaconda3-2019.03-Linux-x86_64.sh
-bash Anaconda3-2019.03-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
+wget -c https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+bash Anaconda3-2019.10-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
