66e13f59362002343d68caca5bae8056ffd4e8fd
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    Added buffalo to golang-packages.sh

--- setup-helpers/golang-packages.sh
+++ setup-helpers/golang-packages.sh
@@ -10,6 +10,9 @@ gometalinter --install
 printf "\n\nxsdgen - structs from XSD\n\n"
 go get -u -v -t aqwari.net/xml/cmd/xsdgen
 
+printf "\n\nBuffalo - API Development in Go\n\n"
+go get -u -v -t github.com/gobuffalo/buffalo/buffalo
+
 printf "\n\nprotoc-gen-go\n\n"
 go get -u -v -t github.com/golang/protobuf/protoc-gen-go
 printf "\n\nprotoc-gen-grpc-gateway\n\n"
