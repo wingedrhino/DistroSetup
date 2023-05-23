87c135a1810395b73ff6dfffcdb169a7f84cb1da
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:12 +0530

    Fedora: Added another system update at the start

--- Fedora/setup.sh
+++ Fedora/setup.sh
@@ -22,6 +22,9 @@ echo "Enable VS Code Repos"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
+echo "First system update"
+sudo dnf update --refresh
+
 echo "Install New Software"
 sudo dnf install \
   NetworkManager-tui \
