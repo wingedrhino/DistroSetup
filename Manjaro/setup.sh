fc969d857168f923a1ef02cd8dcdb07d33123192
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Manjaro: Fixed bug where groups weren't installed
    * I wasn't checking the list against a list of available groups

--- Manjaro/setup.sh
+++ Manjaro/setup.sh
@@ -9,7 +9,9 @@ echo "We'd like to install the following list of workstation packages:"
 sort workstation.list | tee workstation.tmplist
 echo "We'd like to install these packages overall:"
 cat server.list workstation.list | sort | tee merged.tmplist
-pacman -Slq | sort > pacman-available.tmplist
+pacman -Slq | sort > pacman-packages.tmplist
+pacman -Sg | sort > pacman-groups.tmplist
+cat pacman-packages.tmplist pacman-groups.tmplist | sort > pacman-available.tmplist
 echo "The following packages will be installed (after excluding unavaiable ones):"
 comm -12 pacman-available.tmplist merged.tmplist | tee final-install.tmplist
 echo "These packages were skipped because no installation candidates were available for `uname -m` architecture"
