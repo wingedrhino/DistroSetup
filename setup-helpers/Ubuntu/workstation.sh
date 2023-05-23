7b0f652c1cdb1f2f3d2f11049cc2716a8f342d6a
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Added fonts-comic-neue to ubuntu setup
    * The desktop Ubuntu setup is still very much a work in progress

--- setup-helpers/Ubuntu/workstation.sh
+++ setup-helpers/Ubuntu/workstation.sh
@@ -3,6 +3,15 @@
 printf "Begin Ubuntu 18.04 Workstation x86_64 Setup\n"
 printf "\n\nEnsure you've already run server.sh\n"
 
-# TODO add content here
+# Note: You'd probably be better off setting up Ubuntu Workstation starting with
+# installing Ubuntu Studio, instead of starting with installing Ubuntu Desktop.
+# Studio has all the media production tools already setup for you and it runs
+# Xfce, which is the better Desktop Environment.
+
+sudo apt install \
+  fonts-comic-neue \
+  -y
+
+# TODO add more content here
 
 printf "\n\nFinished setup of Ubuntu 18.04 Workstation x86_64!\n\n"
