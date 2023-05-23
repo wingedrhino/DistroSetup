f86044125f994eb10c65eb798780f60ac411309e
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    Added bundle to rubygems.sh setup script

--- setup-helpers/rubygems.sh
+++ setup-helpers/rubygems.sh
@@ -1,13 +1,20 @@
+echo "\n\n"
 echo "Begin: Setup Ruby Gems"
 echo "\n\n"
 
+echo "\n\n"
+echo "Install Bundle for gem management"
+echo "\n\n"
+gem install bundle
+
+echo "\n\n"
 echo "Install IDE tools (for vscode) - ruby-debug-ide, debase, rcodetools"
 echo "\n\n"
-
 gem install ruby-debug-ide
 gem install debase
 gem install rcodetools
 
 echo "\n\n"
 echo "End: Setup Ruby Gems"
+echo "\n\n"
 
