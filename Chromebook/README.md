33adce22729e0997a1abe66d5dfae1b7bde21c6c
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:12 +0530

    WIP Automation for Chromebook
    * This now uses the pythonic APT API via the `apt` package
    * We'll re-use this for Ubuntu automation
    * We'll also be switching to Pythonic dnf/pacman for
      Arch/Manjaro/Fedora/CentOS

--- Chromebook/README.md
+++ Chromebook/README.md
@@ -4,3 +4,11 @@
 
 This repository contains setup instructions for a Chromebook.
 
+## Script Execution
+
+Make sure python3 and python-apt are installed.
+
+```bash
+sudo apt install python3 python-apt
+python3 setup.py
+```
--- Chromebook/setup.py
+++ Chromebook/setup.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 
 import subprocess
+import apt
+import sys
 
-list = [
+cache = apt.cache.Cache()
+cache.update()
+cache.open()
+
+packages = [
         'git',
         'curl',
         'wget',
+        'software-properties-common',
         'build-essential',
         'automake',
         'libtool',
@@ -29,4 +36,23 @@ list = [
         'iotop',
         'nethogs',
         'aptitude',
-    ]
+        'udisks2',
+        'parted',
+        'gparted',
+        'udisks2-lvm2',
+        'udisks2-vdo',
+        'udisks2-zram',
+        'udisks2-btrfs',
+        'udisks2-doc',
+        'default-jdk',
+        'leiningen',
+        'clojure',
+]
+
+for pkg_name in packages:
+    pkg = cache[pkg_name]
+    if pkg.is_installed:
+        print(f'{pkg_name} is already installed.')
+    else:
+        print(f'{pkg_name} will be marked for installation.') 
+
