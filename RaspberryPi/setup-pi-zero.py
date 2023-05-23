cb7c3e51e0542d776220c38e25ed00902f020b94
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    More under-construction Pi setup stuff

--- RaspberryPi/setup-pi-zero.py
+++ RaspberryPi/setup-pi-zero.py
@@ -2,7 +2,6 @@
 
 #
 # NOTE: This script is a work in progress
-# TODO: Check if steps are already done before executing them
 # TODO: Mount/unmount partitions using the udiskie package
 #
 
@@ -24,14 +23,11 @@ def check_partition(location: Path, purpose: str) -> bool:
     return True
 
 
-def auto_detect_root_boot() -> [str, str]:
-    print('Attempting to determine locations automatically...')
-    base_location_str = os.getenv('BASEDIR')
-    if not base_location_str:
-        print('Environment variable "BASEDIR" is empty! Set this to "/media/yourname"')
-        exit(1)
-    base_location = Path(base_location_str)
-    print(f'Checking removable media at {base_location}')
+def auto_detect_root_boot(username: str) -> [str, str]:
+    print('Attempting to find boot and root file systems automatically.')
+    print(f'If you mounted them via your filesystem as the user {username}, this should work!')
+    base_location = Path('/media').joinpath(username)
+    print(f'Checking for directories at {base_location}')
     boot_partition = base_location.joinpath('boot')
     root_partition = base_location.joinpath('rootfs')
     if not check_partition(boot_partition, 'boot'):
@@ -51,21 +47,44 @@ def ensure_ssh(bootfs: Path) -> None:
 
 def ensure_ethernet_gadget(bootfs: Path) -> None:
     print('Ensuring Ethernet Gadget Mode is Enabled')
+
+    # First modify config.txt
+    cfg_entry = 'dtoverlay=dwc2'
     cfg_path = bootfs.joinpath('config.txt')
-    with open(cfg_path, 'a') as cfg_file:
-        cfg_file.write('dtoverlay=dwc2')
+    print(f'Checking for {cfg_entry} in {cfg_path}')
+    with open(cfg_path, 'r') as cfg_file:
+        cfg = cfg_file.read()
+    if cfg_entry not in cfg:
+        print(f'Did not find {cfg_entry} in {cfg_path}. Adding it now...')
+        with open(cfg_path, 'a') as cfg_file:
+            cfg_file.write(cfg_entry)
+    else:
+        print(f'Found {cfg_entry} in {cfg_path}. Nothing to do here!')
+
+    # Next modify cmdline.txt
+    add_cmdline = 'rootwait modules-load=dwc2,g_ether '
     cmdline_path = bootfs.joinpath('cmdline.txt')
+    print(f'Checking for {add_cmdline} in {cmdline_path}')
     with open(cmdline_path, 'r') as cmdline_file:
         cmdline = cmdline_file.read()
-    cmdline = cmdline.replace('rootwait ', 'rootwait modules-load=dwc2,g_ether ')
-    with open(cmdline_path, 'w') as cmdline_file:
-        cmdline_file.write(cmdline)
+    if add_cmdline not in cmdline:
+        print(f'Did not find {add_cmdline} in {cmdline_path}. Adding it now...')
+        cmdline = cmdline.replace('rootwait ', 'rootwait modules-load=dwc2,g_ether ')
+        with open(cmdline_path, 'w') as cmdline_file:
+            cmdline_file.write(cmdline)
+    else:
+        print(f'Found {add_cmdline} in {cmdline_path}. Nothing to do here!')
+
+
+def ensure_wifi(bootfs: Path) -> None:
+    pass
 
 
 def setup() -> None:
     print('Setting up a Raspberry Pi Zero(-W(H))')
     print('This script only runs on Linux!')
-    _, bootfs = auto_detect_root_boot()
+    username = os.getenv('USERNAME')
+    rootfs, bootfs = auto_detect_root_boot(username)
     if not bootfs:
         exit(1)  # bail out
     ensure_ssh(bootfs)
