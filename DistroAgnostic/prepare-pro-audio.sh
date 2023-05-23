c4a1142285834ea8d554e12ee441bcfc2ca46052
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:12 +0530

    Updates to prepare-pro-audio.sh

--- DistroAgnostic/prepare-pro-audio.sh
+++ DistroAgnostic/prepare-pro-audio.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+echo "Current ulimit"
+ulimit -l
+echo "setting ulimit to unlimited"
+sudo ulimit -l unlimited
 echo "Setting up a few things that'd make Pro Audio Nicer"
+echo "Setting Max User Watchers to 524288"
 sudo sysctl -w fs.inotify.max_user_watches=524288
+echo "Setting Swappiness to 1"
 sudo sysctl -w vm.swappiness=1
+echo "Setting hpet"
 sudo sysctl -w dev.hpet.max-user-freq=3072
 echo -n 3072 | sudo tee /sys/class/rtc/rtc0/max_user_freq
 sudo chmod 660 /dev/hpet /dev/rtc0
