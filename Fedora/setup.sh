91bc4153e000d965c23d4f877ac959eda486b73e
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:12 +0530

    Fedora: Cleaned up Nodesource Node installs

--- Fedora/setup.sh
+++ Fedora/setup.sh
@@ -22,9 +22,6 @@ echo "Enable Brave Repos"
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc 
 
-# echo "Enable NodeSource Node.js 17.x Repos"
-# curl -sL https://rpm.nodesource.com/setup_17.x | sudo bash -
-
 echo "Enable VS Code Repos"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
