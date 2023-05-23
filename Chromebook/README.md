63fef3c2f9da34b41ed644b218059ae2c61e0ab9
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:12 +0530

    Added WIP Chromebook Setup

---
+++ Chromebook/README.md
@@ -0,0 +1,6 @@
+# Chromebook Setup
+
+`Note: WIP!`
+
+This repository contains setup instructions for a Chromebook.
+
---
+++ Chromebook/setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import subprocess
+
+list = [
+        'git',
+        'curl',
+        'wget',
+        'build-essential',
+        'automake',
+        'libtool',
+        'autoconf',
+        'pkg-config',
+        'udev',
+        'fuse',
+        'snap',
+        'snapd',
+        'zsh',
+        'byobu',
+        'python3',
+        'libsquashfuse0',
+        'squashfuse',
+        'fuse',
+        'vim',
+        'atop',
+        'zsh',
+        'byobu',
+        'htop',
+        'iotop',
+        'nethogs',
+        'aptitude',
+    ]
