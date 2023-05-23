d53b9b5b1d1af9ccb9090230ec5315bab5dc5df1
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Added VSCode to setup.sh

--- setup-helpers/Fedora/setup.sh
+++ setup-helpers/Fedora/setup.sh
@@ -15,6 +15,10 @@ dnf config-manager \
   --add-repo \
   https://download.docker.com/linux/fedora/docker-ce.repo
 
+printf "\n\nEnable VSCode Repo\n"
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
 printf "\n\nRefresh newly added repos via dnf update --refresh\n"
 dnf update --refresh
 
@@ -81,6 +85,7 @@ dnf install \
   cheese \
   xfce4-sensors-plugin \
   redshift-gtk \
+  code \
   pcmanfm-qt
 
 
