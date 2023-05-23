ba8aa20dfb3e2a2d2f2f3d1d2653abe2439529e7
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:11 +0530

    Manjaro: Move packages from Snap to Arch Repos / AUR
    * Makes more sense for opensource packages to be installed from AUR if
      there is a stable version in AUR!

---
+++ Manjaro/snaps.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "Snap: Olive Video Editor"
+sudo snap install --edge olive-editor
+echo "Snap: Insomnia"
+sudo snap install insomnia	
+echo "Snap: Slack"
+sudo snap install slack --classic
+echo "Snap: Discord"
+sudo snap install discord
+echo "Snap: Zoom"
+sudo snap install zoom-client
