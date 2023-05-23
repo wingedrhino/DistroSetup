1d293c7182846394572387e1f009944fb870f451
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Ubuntu: Updated studio.sh

--- Ubuntu/studio.sh
+++ Ubuntu/studio.sh
@@ -3,10 +3,11 @@
 echo "Begin Ubuntu 20.04 Studio Setup"
 echo "Ensure you've already run server.sh and workstation.sh"
 
-echo"Setting up OBS Studio PPA"
-add-apt-repository ppa:obsproject/obs-studio -y
+echo "Setting up OBS Studio PPA"
+sudo add-apt-repository ppa:obsproject/obs-studio -y
 
 apt install \
+  ubuntustudio-installer \
   fonts-comic-neue \
   v4l-utils \
   v4l2loopback-dkms \
@@ -15,7 +16,7 @@ apt install \
   -y
 
 echo "Finall Apt Autoremove"
-apt autoremove -y
+sudo apt autoremove -y
 
 echo "Finished Ubuntu 20.04 Studio Setup!"
 
