18808a211ca254e139818354edfdee03a456f9ce
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Manjaro: Ignore unavailable packages
    * Did some magic with the comm command to avoid installing those
      packages that are unavailable

--- Manjaro/workstation.sh
+++ Manjaro/workstation.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
-echo "The following server packages will be installed:"
-cat server.list
-echo "The following workstation packages will be installed:"
-cat workstation.list
-cat workstation.list server.list | sudo pacman -Syu --needed -
-
+# Thanks to this awesome trick for this script:
+# https://wiki.archlinux.org/index.php/Pacman/Tips_and_tricks#Install_packages_from_a_list
+echo "We'd like to install the following list of server packages:"
+sort server.list | tee server.tmplist
+echo "We'd like to install the following list of workstation packages:"
+sort workstation.list | tee workstation.tmplist
+echo "We'd like to install these packages overall:"
+cat server.list workstation.list | sort | tee merged.tmplist
+pacman -Slq | sort > pacman-available.tmplist
+echo "The following packages will be installed (after excluding unavaiable ones):"
+comm -12 pacman-available.tmplist merged.tmplist | tee final-install.tmplist
+echo "These packages were skipped because no installation candidates were available for `uname -m` architecture"
+comm -23 final-install.tmplist merged.tmplist | tee skipped.tmplist
+echo "Running pacman -Syu and installing packages now..."
+sudo pacman -Syu --needed < final-install.tmplist
+echo "Cleaning up temporary files..."
+rm *.tmplist
+echo "Done! Now you may proceed to install snaps"
