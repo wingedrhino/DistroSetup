ab502221a4477c594dadb8b06aa96e706d1aebe1
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Setup Helpers: Bump Anaconda to 5.3.0 from 5.1.0
    PS: All these days I thought something was wrong with Anaconda's website
    and it turns out that they just changed domains from continuum.io to
    anaconda.com.

--- setup-helpers/Fedora/as-user.sh
+++ setup-helpers/Fedora/as-user.sh
@@ -24,7 +24,7 @@ curl -L https://raw.githubusercontent.com/wingedrhino/DistroSetup/trunk/dotfiles
 
 printf "\n\nInstall Anaconda\n"
 rm -r $HOME/ext/bin/anaconda
-wget -c https://repo.anaconda.com/archive/Anaconda3-5.1.0-Linux-x86_64.sh
-chmod a+x ./Anaconda3-5.1.0-Linux-x86_64.sh
-bash ./Anaconda3-5.1.0-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
+wget -c https://repo.anaconda.com/archive/Anaconda3-5.3.0-Linux-x86_64.sh
+chmod a+x ./Anaconda3-5.3.0-Linux-x86_64.sh
+bash ./Anaconda3-5.3.0-Linux-x86_64.sh -b -p $HOME/ext/bin/anaconda
 
