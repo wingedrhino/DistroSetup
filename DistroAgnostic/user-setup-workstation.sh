4ad5026f3a6af00de9a34f685ab585e2de83d87a
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:11 +0530

    Add user to adbusers

--- DistroAgnostic/user-setup-workstation.sh
+++ DistroAgnostic/user-setup-workstation.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-echo "Add user to docker, audio, and realtime groups"
+echo "Add user to various groups"
 sudo gpasswd -a $USER vboxusers
 sudo gpasswd -a $USER audio
 sudo gpasswd -a $USER realtime
 sudo gpasswd -a $USER docker
 sudo gpasswd -a $USER uucp
+sudo gpasswd -a $USER adbusers
 
 balooctl stop
 balooctl disable
