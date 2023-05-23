86f6b94e4af3590cc587a54583335d5518131910
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:09 +0530

    Added comment for minikube in Fedora setup

--- setup-helpers/Fedora/dev-server-setup.sh
+++ setup-helpers/Fedora/dev-server-setup.sh
@@ -36,6 +36,7 @@ printf "\n\nInstall docker-compose\n"
 curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
+printf "\n\nInstall minikube\n"
 curl -L https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 -o /usr/local/bin/minikube
 chmod +x /usr/local/bin/minikube
 
