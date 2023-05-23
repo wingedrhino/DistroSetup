25a08f5cf9eeed2ae6b11d4077da836f1baa2219
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    Removed extra stuff from golang-packages.sh
    * TODO maybe add gometalinter later

--- setup-helpers/golang-packages.sh
+++ setup-helpers/golang-packages.sh
@@ -6,69 +6,9 @@ printf "\n\n"
 go get -u -v -t github.com/golang/dep/cmd/dep
 
 printf "\n\n"
-printf "delve - a debugger for Go"
+printf "xsdgen; make sure you remove UseFieldNames() from config and recompile"
 printf "\n\n"
-go get -u -v -t github.com/derekparker/delve/cmd/dlv
-
-printf "\n\n"
-printf "gocode - an autocompletion daemon for Go"
-printf "\n\n"
-go get -u -v -t github.com/nsf/gocode
-
-printf "\n\n"
-printf "godef - prints where symbols are defined in Go source code"
-printf "\n\n"
-go get -u -v -t github.com/rogpeppe/godef
-
-printf "\n\n"
-printf "gogetdoc - gets documentation for items in Go source code"
-printf "\n\n"
-go get -u -v -t github.com/zmb3/gogetdoc
-
-printf "\n\n"
-printf "goimports - updates your import lines, adds missing ones, removes unreferenced ones"
-printf "\n\n"
-go get -u -v -t golang.org/x/tools/cmd/goimports
-
-printf "\n\n"
-printf "golint - the lint tool developed by Google for Go"
-printf "\n\n"
-go get -u -v -t github.com/golang/lint/golint
-
-printf "\n\n"
-printf "gometalinter - concurrently run go lint tools and normalize their output"
-printf "\n\n"
-go get -u -v -t github.com/alecthomas/gometalinter
-
-printf "\n\n"
-printf "gorename - precise, type-safe renaming of identifiers in Go source code"
-printf "\n\n"
-go get -u -v -t golang.org/x/tools/cmd/gorename
-
-printf "\n\n"
-printf "goreturns - a gofmt/goimports-like tool for Go programmers that fills in Go return statements with zero values to match the func return types"
-printf "\n\n"
-go get -u -v -t github.com/sqs/goreturns
-
-printf "\n\n"
-printf "guru - a tool for answering questions about Go source code"
-printf "\n\n"
-go get -u -v -t golang.org/x/tools/cmd/guru
-
-printf "\n\n"
-printf "gopkgs - list your installed Go packages for import."
-printf "\n\n"
-go get -u -v -t github.com/tpng/gopkgs
-
-printf "\n\n"
-printf "go-outline - Utility to extract JSON representation of declarations from a Go source file"
-printf "\n\n"
-go get -u -v -t github.com/lukehoban/go-outline
-
-printf "\n\n"
-printf "go-langserver - Go language server to add Go support to editors and other tools that use the Language Server Protocol (LSP)"
-printf "\n\n"
-go get -u -v -t github.com/sourcegraph/go-langserver
+go get -u -v -t aqwari.net/xml/cmd/xsdgen
 
 printf "\n\n"
 printf "protoc-gen-go"
