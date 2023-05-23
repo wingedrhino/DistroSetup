1f43776cf6897e796933f20d34c43b137fcc3539
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Updated Python 3.8 Install Script

--- Ubuntu/python38.sh
+++ Ubuntu/python38.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 echo "Installing Python 3.8 into Ubuntu 18.04"
-apt install -y python3.8 python3.8-venv
+sudo apt install -y python3.8 python3.8-venv
+python3.8 -m pip install --user pipx
+pipx install youtube-dl
+pipx install mypy
