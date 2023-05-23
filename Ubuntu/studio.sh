80444e0c0af381c29a451440637978ba791338f2
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:10 +0530

    Updated Ubuntu Studio Setup Scripts

--- Ubuntu/studio.sh
+++ Ubuntu/studio.sh
@@ -6,8 +6,23 @@ echo "Ensure you've already run server.sh and workstation.sh"
 echo "Setting up OBS Studio PPA"
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 
+echo "Setting up Ubuntu Studio Backports PPA"
+sudo add-apt-repository ppa:ubuntustudio-ppa/backports -y
+
 apt install \
+  linux-lowlatency \
   ubuntustudio-installer \
+  ubuntustudio-menu \
+  ubuntustudio-wallpapers \
+  ubuntustudio-branding-common \
+  ubuntustudio-audio \
+  ubuntustudio-graphics \
+  ubuntustudio-photography \
+  ubuntustudio-publishing \
+  ubuntustudio-video \
+  ubuntustudio-lowlatency-settings \
+  ubuntustudio-performance-tweaks \
+  carla \
   fonts-comic-neue \
   v4l-utils \
   v4l2loopback-dkms \
