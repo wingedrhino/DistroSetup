783f339805c2d01c15c6c7592f647276b07cbdd7
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    atom-packages.sh setup helper
    * Installs currently used atom packages to a fresh Atom install
    * As of this commit, file still incomplete

---
+++ setup-helpers/atom-packages.sh
@@ -0,0 +1,19 @@
+echo "Start: Install Atom Packages"
+
+echo "Linter - Base Linter"
+apm install linter
+
+echo "Nuclide - Facebook's Frontend Kit"
+apm install nuclide
+
+echo "Go Setup - go-plus, go-debug, go-signature-statusbar"
+apm install go-plus
+apm install go-debug
+apm install go-signature-statusbar
+
+echo "Python Setup - atom-jinja2, autocomplete-python, linter-pylint"
+apm install atom-jinja2
+apm install autocomplete-python
+apm install linter-pylint
+
+echo "End: Install Atom Packages"
