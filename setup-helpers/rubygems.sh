7b3a44d5ab7ef48e7cf310a9979a6664759d28f9
wingedrhino <noreply@wingedrhino.com>
2023-05-24 04:21:08 +0530

    Added rubygems.sh setup helper

---
+++ setup-helpers/rubygems.sh
@@ -0,0 +1,13 @@
+echo "Begin: Setup Ruby Gems"
+echo "\n\n"
+
+echo "Install IDE tools (for vscode) - ruby-debug-ide, debase, rcodetools"
+echo "\n\n"
+
+gem install ruby-debug-ide
+gem install debase
+gem install rcodetools
+
+echo "\n\n"
+echo "End: Setup Ruby Gems"
+
