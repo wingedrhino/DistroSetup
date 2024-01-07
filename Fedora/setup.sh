bfb959da3a1b48f5011defc5ec7086f168561dcd
wingedrhino <noreply@wingedrhino.com>
2024-01-08 04:40:33 +0530

    Fedora: Added vlc plugins
    * vlc-plugin-ffmpeg
    * vlc-plugin-gnome
    * vlc-plugin-pipewire
    * vlc-plugins-video-out
    * vlc-plugin-fluidsynth

--- Fedora/setup.sh
+++ Fedora/setup.sh
@@ -108,6 +108,11 @@ sudo dnf install \
   gnome-shell-extension-system-monitor-applet \
   gnome-shell-extension-workspace-indicator \
   vlc \
+  vlc-plugin-ffmpeg \
+  vlc-plugin-gnome \
+  vlc-plugin-pipewire \
+  vlc-plugins-video-out \
+  vlc-plugin-fluidsynth \
   hexchat \
   deluge \
   gparted \
