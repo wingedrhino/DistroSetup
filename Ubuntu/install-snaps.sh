60342814fe35755e66ba1cb1fc14c22860656e8f
wingedrhino <noreply@wingedrhino.com>
2024-06-17 21:28:23 +0530

    Updated Redis in Ubuntu snaps
    * The package name had changed

--- Ubuntu/install-snaps.sh
+++ Ubuntu/install-snaps.sh
@@ -3,6 +3,10 @@ echo "Snap: doctl"
 sudo snap install doctl
 echo "Snap: Chromium"
 sudo snap install chromium
+echo "Snap: Redis Desktop Manager"
+sudo snap install redisinsight
+echo "Snap: Insomnia"
+sudo snap install insomnia
 echo "Snap: Eclipse"
 sudo snap install eclipse --classic
 echo "Snap: Android Studio"
@@ -11,8 +15,4 @@ echo "Snap: PyCharm"
 sudo snap install pycharm-community --classic
 echo "Snap: IntelliJ"
 sudo snap install intellij-idea-community --classic
-echo "Snap: Redis Desktop Manager"
-sudo snap install redis-desktop-manager
-echo "Snap: Insomnia"
-sudo snap install insomnia
 
